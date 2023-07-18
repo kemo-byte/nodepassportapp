713431c5c39ed2fe14ad5e61acb247e959d090f1
kamal kafi <kamalkafi12@gmail.com>
2023-07-18 10:18:20 +0200

    add upload model

---
+++ models/Upload.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref:'User'
+    },
+    name:{
+        type: String,
+        required:[true,'Plase add file name']
+    }
+    
+    
+},{timestamps:true})
+
+const Upload = mongoose.model('Upload',userSchema)
+
+module.exports = Upload
