0bd342f575a23c86388f93422e7e9a9912ce1d7f
kamal kafi <kamalkafi12@gmail.com>
2023-07-19 21:52:20 +0200

    add show photo page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,6 +7,14 @@ const dashboard = async (req,res)=> {
     res.render('dashboard',{user:req.user,photos})
 
 }
+
+
+const show = async (req,res)=> {
+    let query = req.query.id,
+     photo = await uploadModel.find({_id:`${query}`}).sort({_id:-1})
+    res.render('show',{photo})
+
+}
 const welcome = (req,res)=> res.render('welcome')
 
-module.exports = {dashboard, welcome}
+module.exports = {dashboard, welcome, show}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const {ensureAuthenticated} = require('../config/auth')
-const {dashboard, welcome} = require('../controllers/homeController')
+const {dashboard, welcome,show} = require('../controllers/homeController')
 
 // Welcome Page 
 router.get('/', welcome)
 // Dashboard
 
 router.get('/dashboard',ensureAuthenticated,dashboard)
+router.get('/show',ensureAuthenticated,show)
 
 
 module.exports = router
