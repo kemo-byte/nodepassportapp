b354bc504e40d01bd523e11269930904face7018
kamal kafi <kamalkafi12@gmail.com>
2023-07-19 22:05:43 +0200

    add try catch to show and dashboard pages

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,17 +2,29 @@ const uploadModel = require('../models/Upload')
 
 
 const dashboard = async (req,res)=> {
-    let query= {user:req.user.id},
-    photos = await uploadModel.find(query).sort({_id:-1})
+    let query= {user:req.user.id}
+    try{
+    let photos = await uploadModel.find(query).sort({_id:-1})
     res.render('dashboard',{user:req.user,photos})
+
+    } catch(err) {
+        console.log(err);
+        res.redirect('/')
+    }
 
 }
 
 
 const show = async (req,res)=> {
-    let query = req.query.id,
-     photo = await uploadModel.find({_id:`${query}`}).sort({_id:-1})
-    res.render('show',{photo})
+    let query = req.query.id
+    try {
+        let photo = await uploadModel.find({_id:`${query}`}).sort({_id:-1})
+        res.render('show',{photo})
+
+    } catch (err) {
+        console.log(err);
+        res.redirect('/dashboard')
+    }
 
 }
 const welcome = (req,res)=> res.render('welcome')
